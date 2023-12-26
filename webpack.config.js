f74b99f1971cd2b633c8934f943d694b4733b397
bakum-bicum <carla.devjs@gmail.com>
2023-12-26 13:13:46 -0300

    Adiciona dependencias de desenvolvimento

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+	entry: './src/js/main.js',
+	output: {
+		path: path.resolve(__dirname, './build/js'),
+		filename: 'script.js',
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['@babel/preset-env'],
+						plugins: ['@babel/plugin-transform-runtime'],
+					},
+				},
+			},
+		],
+	},
+};
