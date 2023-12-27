696efbe744c5f14f11a7b9f54d10a6fcf96a8f8f
bakum-bicum <carla.devjs@gmail.com>
2023-12-27 08:33:25 -0300

    Cria pasta components

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,4 @@
-import SmoothScroll from './SmoothScroll.js';
+import SmoothScroll from './components/SmoothScroll.js';
 
 const smoothScroll = new SmoothScroll('.main-nav__list', { behavior: 'smooth', block: 'start' });
 smoothScroll.init();
